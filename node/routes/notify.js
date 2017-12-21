35c7e54fc8f4db13ef068685597d7849aad4e211
shnavy <shuaihj@hztywl.cn>
2017-12-21 14:13:46 +0800

    2017年12月21日 星期四 14时13分46秒 CST

--- node/routes/notify.js
+++ node/routes/notify.js
@@ -1,9 +1,43 @@
 const router = require('koa-router')()
+const knex = require('../utils/knexfile')
+const request = require('superagent')
+const crypto = require('crypto')
 
 router.post('/', async (ctx, next) => {
-  let body = ctx.request.body
-  console.log('body', body)
-  ctx.body = body
+  ctx.body = `<xml>
+                <return_code><![CDATA[SUCCESS]]></return_code>
+                <return_msg><![CDATA[OK]]></return_msg>
+              </xml>`
+  let body = ctx.request.body.xml
+  //查询订单
+  let order_no = body.out_trade_no[0]
+  let order = await knex('recharge').where({ order_no }).first()
+  let app = await knex('app').where({ wx_appid: body.appid[0] }).first()
+  //退款通知
+  if (body.req_info){
+    let stringA = new Buffer(body.req_info[0], 'base64').toString()
+    let md5sum = crypto.createHash('md5')
+    md5sum.update(app.api_key)
+    let key = md5sum.digest('hex').toLowerCase()
+    decipher = crypto.Decipher('aes-256-cbc', key)
+    // 使用BASE64对密文进行解码，然后AES-CBC解密
+    // decipher.setAutoPadding(false)
+    let msg = decipher.update(stringA, 'base64', 'utf8') + decipher.final('utf8')
+    console.log('msg', msg)
+
+  }else{
+    if (body.return_code[0] == 'SUCCESS') {
+      if(order.status == 'pending'){
+        await knex('recharge').update({ status: 'paid', paid_at: new Date()})
+        await request('POST', app.notify_url).send({ "type": "charge.succeeded", order_no, openid: body.openid[0], amount: body.total_fee[0]})
+      }else {
+        //不做任何处理
+      }
+    }else{
+      await knex('recharge').update({ status: 'failed', failure_msg: body.return_msg[0]})
+      await request('POST', app.notify_url).send({ "type": "charge.failed", order_no, failure_msg: body.return_msg[0]})
+    }
+  }
 })
 
 module.exports = router
