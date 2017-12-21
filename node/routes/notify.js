d5ec19686699971f8c5c432a5cf7496cbaee47ff
shnavy <shuaihj@hztywl.cn>
2017-12-21 16:32:45 +0800

    2017年12月21日 星期四 16时32分45秒 CST

--- node/routes/notify.js
+++ node/routes/notify.js
@@ -19,7 +19,7 @@ router.post('/', async (ctx, next) => {
     console.log('key', key)
     let decipher = crypto.createDecipher('aes-256-ecb', key)
     // 使用BASE64对密文进行解码，然后AES-CBC解密
-    // decipher.setAutoPadding(false)
+    decipher.setAutoPadding(false)
     let msg = decipher.update(stringA, 'base64', 'utf8')
     msg += decipher.final('utf8')
     console.log('msg', msg)
