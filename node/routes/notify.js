871b53ea9c9026797d8fd116d2f720b07f5a3dc8
shnavy <shuaihj@hztywl.cn>
2017-12-21 11:58:29 +0800

    2017年12月21日 星期四 11时58分29秒 CST

--- node/routes/notify.js
+++ node/routes/notify.js
@@ -2,7 +2,8 @@ const router = require('koa-router')()
 
 router.post('/', async (ctx, next) => {
   let body = ctx.request.body
-  console.log('body', ctx)
+  console.log('body', body)
+  ctx.body = body
 })
 
 module.exports = router
